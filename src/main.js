75a775e029afa07d73983701c79a6044f4eda4e5
fzh.Mollaei <fe.mollaei@gmail.com>
2026-07-11 19:53:22 +0330

    feat: configure vue-toastification and global styles

--- src/main.js
+++ src/main.js
@@ -3,11 +3,25 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-// import './styles/main.scss'
 import '@/styles/main.scss'
+
+import 'vue-toastification/dist/index.css'
+import Toast, { POSITION } from 'vue-toastification'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Toast, {
+  position: POSITION.BOTTOM_RIGHT,
+  timeout: 2000,
+  closeButton: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  toastClassName: 'app-toast',
+  bodyClassName: 'app-toast-body',
+  transition: 'Vue-Toastification__fade',
+  transitionDuration: 800,
+})
 
 app.mount('#app')
