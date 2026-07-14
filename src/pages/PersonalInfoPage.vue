fb94d44977fc5aa406b065e9ae3cb3547a2eb2f7
fzh.Mollaei <fe.mollaei@gmail.com>
2026-07-14 20:08:19 +0330

    feat: add profile data saving logic and validation to personal page

--- src/pages/PersonalInfoPage.vue
+++ src/pages/PersonalInfoPage.vue
@@ -1,13 +1,30 @@
 <script setup>
 import Button from '@/components/Button.vue'
 import Input from '@/components/Input.vue'
+import { useUserStore } from '@/stores/userStore'
+import { registerValidationRules } from '@/validation/validationRules'
+import { useRouter } from 'vue-router'
+import { Form } from 'vee-validate'
+
+registerValidationRules()
+
+const router = useRouter()
+const userStore = useUserStore()
+
+function saveData(userData) {
+  userStore.saveInfo(userData)
+  router.push({ name: 'upload-id' })
+}
+function goBack() {
+  router.push({ name: 'dashboard' })
+}
 </script>
 
 <template>
   <div class="page">
     <h1 class="page__title">اطلاعات فردی</h1>
     <div class="divider"></div>
-    <form class="page__form">
+    <Form @submit="saveData" class="page__form">
       <div class="page__form-row">
         <Input
           name="firstName"
@@ -28,7 +45,7 @@ import Input from '@/components/Input.vue'
           label="کدپستی"
           placeHolder="کدپستی 10 رقمی"
           variant="personal"
-          rules="required:کدپستی|tenDigit"
+          rules="required:کدپستی"
         />
       </div>
       <Input
@@ -40,13 +57,13 @@ import Input from '@/components/Input.vue'
         rules="required:آدرس محل سکونت"
       />
       <div class="page__form-buttons">
-        <Button variant="secondary">
+        <Button @press="goBack" variant="secondary">
           <template #btnLabel>قبلی</template>
         </Button>
         <Button type="submit">
           <template #btnLabel>ثبت و ادامه</template>
         </Button>
       </div>
-    </form>
+    </Form>
   </div>
 </template>
