77cf9f661cec03a2a94cc723f10a6946e20b41aa
fzh.Mollaei <fe.mollaei@gmail.com>
2026-08-01 23:19:55 +0330

    feat: add ActionMenu component

---
+++ src/components/ActionMenu.vue
@@ -0,0 +1,63 @@
+<script setup>
+defineProps({ menuItems: { type: Array }, required: true })
+const emit = defineEmits(['select'])
+</script>
+<template>
+  <div class="action-menu">
+    <div v-for="(item, index) in menuItems" :key="item.label">
+      <button
+        @click="emit('select', item.action)"
+        class="action-menu__item"
+        :class="{ 'action-menu__item--disabled': item.disabled }"
+      >
+        <img class="action-menu__icon" :src="item.icon" />
+        <span
+          class="action-menu__label"
+          :class="{
+            'action-menu__label--danger': item.danger,
+            'action-menu__label--disabled': item.disabled,
+          }"
+          >{{ item.label }}</span
+        >
+      </button>
+      <div v-if="index !== menuItems.length - 1" class="action-menu__divider"></div>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+@use '@/styles/mixins' as *;
+.action-menu {
+  @include flex(column, flex-start, stretch);
+  box-shadow: 0px 0px 4px 0px #0043650d;
+  background-color: #ffffff;
+  padding: 12px;
+  border-radius: 12px;
+  gap: 12px;
+  &__item {
+    @include flex(row, flex-start, center);
+    gap: 8px;
+    background-color: inherit;
+    cursor: pointer;
+    &--disabled {
+      pointer-events: none;
+    }
+  }
+  &__label {
+    font-weight: 400;
+    font-size: 14px;
+    color: #3c4351;
+    &--danger {
+      color: #eb482b;
+    }
+    &--disabled {
+      color: #c3c5c9;
+      pointer-events: none;
+    }
+    &__divider {
+      height: 1px;
+      background-color: #e2edff;
+      margin-top: 12px;
+    }
+  }
+}
+</style>
--- src/components/Button.vue
+++ src/components/Button.vue
@@ -2,7 +2,7 @@
 const props = defineProps({
   type: { type: String, default: 'button' },
   variant: { type: String, default: 'primary' },
-  heightBtn: { type: String, default: '58px' },
+  heightBtn: { type: String, default: '48px' },
   disabled: { type: String, default: false },
 })
 </script>
