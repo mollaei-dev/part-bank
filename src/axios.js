30dc3f4a71b8ad847718d82169b3e05d72121880
fzh.Mollaei <fe.mollaei@gmail.com>
2026-08-06 23:25:21 +0330

    refactor: use axios interceptor for token injection

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,14 @@ const api = axios.create({
   baseURL: 'https://turbofront5.onrender.com',
   timeout: 5000,
   headers: {
-    'context-type': 'application.json',
+    'content-type': 'application.json',
   },
+})
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
 })
 export default api
