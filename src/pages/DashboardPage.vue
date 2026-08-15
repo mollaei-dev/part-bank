a6156bbe6969573a4356c5f712f297eac1439aab
fzh.Mollaei <fe.mollaei@gmail.com>
2026-08-15 17:12:31 +0330

    feat: integrate Sidebar into dashboard view

--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -1,5 +1,22 @@
-<style lang="scss" scoped></style>
+<script setup>
+import Sidebar from '@/components/sidebar/Sidebar.vue'
+</script>
 <template>
-
+  <div class="dashboard">
+    <Sidebar />
+    <div class="dashboard__content"></div>
+  </div>
 </template>
-<script setup></script>
+<style lang="scss" scoped>
+.dashboard {
+  margin: 108px auto;
+  display: flex;
+  width: 100%;
+  max-width: 1400px;
+  background-color: #f7f8fa;
+  gap: 16px;
+}
+.dashboard__content {
+  background: #000;
+}
+</style>
