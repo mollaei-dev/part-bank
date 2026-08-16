a6f93450464377738487c8e1c397b02969d6ee96
fzh.Mollaei <fe.mollaei@gmail.com>
2026-08-16 15:59:57 +0330

    feat: implement demo mode for current user

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -3,6 +3,7 @@ import { defineStore } from 'pinia'
 import { useToast } from 'vue-toastification'
 const delay = (ms) => new Promise((res) => setTimeout(res, ms))
 const toast = useToast()
+const demoUser = { firstName: 'محمدجواد', lastName: 'رحمانی نصب', idNumber: '0920125444' }
 
 export const useUserStore = defineStore('user', () => {
   const currentUser = ref(JSON.parse(localStorage.getItem('currentUser')) || null)
@@ -45,6 +46,7 @@ export const useUserStore = defineStore('user', () => {
   async function activeDemoMode() {
     toast.error('خطا در برقراری ارتباط با سرور')
     localStorage.setItem('token', 'demoToken')
+    setCurrentUser(demoUser)
     await delay(3000)
     toast.info('حالت دمو فعال شد')
     await delay(2000)
@@ -57,6 +59,6 @@ export const useUserStore = defineStore('user', () => {
     saveCardImage,
     deleteCardImage,
     activeDemoMode,
-    setHasAccount
+    setHasAccount,
   }
 })
