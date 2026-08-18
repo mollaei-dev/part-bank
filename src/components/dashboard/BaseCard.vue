f851b582817a0c0730aa22112ceacec39b3f71fc
fzh.Mollaei <fe.mollaei@gmail.com>
2026-08-18 16:18:36 +0330

    feat: create BaseCard component

---
+++ src/components/dashboard/BaseCard.vue
@@ -0,0 +1,62 @@
+<script setup>
+import Button from '../Button.vue'
+const props = defineProps(['cardTitle', 'metaIcon', 'metaLabel', 'btnIcon', 'btnLabel'])
+</script>
+<template>
+  <div class="card">
+    <div class="card__header">
+      <div class="card__title">{{ cardTitle }}</div>
+      <div class="card__meta">
+        <span class="card__meta-label">{{ metaLabel }}</span>
+        <img class="card__meta-icon" :src="metaIcon" />
+      </div>
+    </div>
+    <slot name="content"></slot>
+    <div class="card__footer">
+      <slot name="details"></slot>
+      <Button class="card__action" heightBtn="44px" variant="secondary">
+        <template #btnLabel>
+          {{ btnLabel }}
+          <img :src="btnIcon" />
+        </template>
+      </Button>
+    </div> 
+  </div>
+</template>
+<style lang="scss">
+@use '@/styles/mixins' as *;
+.card {
+  flex: 1;
+  width: 338px;
+  min-width: 320px;
+  max-width: 338px;
+  height: 260px;
+  @include flex(column, space-between, stretch);
+  background: #ffffff;
+  border-radius: 12px;
+  padding: 24px;
+  &__header {
+    @include flex(row, space-between, center);
+  }
+  &__title {
+    font-weight: 700;
+    font-size: 20px;
+    color: #3c4351;
+  }
+  &__meta {
+    @include flex(row, flex-start, center);
+    gap: 5px;
+  }
+  &__meta-label {
+    color: #8999b9;
+    font-weight: 400;
+    font-size: 14px;
+  }
+  &__meta-icon {
+  }
+  &__footer {
+    @include flex(column, flex-start, normal);
+    gap: 20px;
+  }
+}
+</style>
