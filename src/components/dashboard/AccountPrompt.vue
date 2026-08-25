7c047e355d3f22ecd6a3fe00f5d96bef8ee29bb7
fzh.Mollaei <fe.mollaei@gmail.com>
2026-08-25 16:30:55 +0330

    feat: create AccountPrompt component and

---
+++ src/components/dashboard/AccountPrompt.vue
@@ -0,0 +1,43 @@
+<script setup>
+import Button from '../Button.vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+function openAccount() {
+  router.push({ name: 'personal-info' })
+}
+</script>
+
+<template>
+  <div class="prompt">
+    <img class="prompt__icon" src="@/assets/images/icons/iconBigSearch.png" />
+    <p class="prompt__text">برای دسترسی به داشبورد، لطفا ابتدا افتتاح حساب کنید</p>
+    <Button class="prompt__button" @press="openAccount">
+      <template #btnLabel>افتتاح حساب</template>
+    </Button>
+  </div>
+</template>
+<style lang="scss">
+@use '@/styles/mixins' as *;
+
+.prompt {
+  position: absolute;
+  z-index: 900;
+  inset: 0;
+  margin: auto;
+  width: 331px;
+  height: 237px;
+  @include flex(column, center, center);
+  padding: 16px 0;
+  gap: 22px;
+  &__text {
+    font-weight: 600;
+    font-size: 16px;
+    color: #3c4351;
+    width: 100%;
+  }
+  &__button {
+    width: 200px;
+  }
+}
+</style>
