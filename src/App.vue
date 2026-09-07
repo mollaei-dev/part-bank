e4ff7578fa0c09d54f62b363b20633d03876e4d4
fzh.Mollaei <fe.mollaei@gmail.com>
2026-09-07 13:19:07 +0330

    feat: add slide-fade transition for route navigation

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,20 @@ import Header from './components/Header.vue'
 </script>
 <template>
   <Header v-if="$route.fullPath !== '/'"></Header>
-  <RouterView></RouterView>
+  <router-view v-slot="{ Component }">
+    <Transition name="slide-fade" mode="out-in">
+      <component :is="Component" :key="$route.fullPath" />
+    </Transition>
+  </router-view>
 </template>
-<style lang="scss" scoped></style>
+<style lang="scss">
+.slide-fade-enter-active,
+.slide-fade-leave-active {
+  transition: all 0.3s ease;
+}
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
+}
+</style>
